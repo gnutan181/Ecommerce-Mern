e287526ecc341727de672dc4736aecbadd53e3a8
gnutan181 <101492564+gnutan181@users.noreply.github.com>
2023-09-04 15:38:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 import colors from "colors";
 mongoose.set('strictQuery', false)
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    );
-  } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
-  }
-};
+const connectDB = async () => {  
+      try {
+          const connect =  await mongoose.connect(process.env.MONGO_URL, {
+              useUnifiedTopology: true,
+              useNewUrlParser : true
+            }) ;
+            
+            // console.log(`Mongodb Connected`)
+            console.log(`Mongodb Connected : ${connect.connection.host}`)
+            
+          } catch (error) {
+            console.log(error);
+            process.exit(1); // if there is no connection between database then we dont want to further processs
+          }
+        }
+ export default connectDB;
 
-export default connectDB;
